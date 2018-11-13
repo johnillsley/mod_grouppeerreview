6fb6ecba4f29d70f0323c26a88970b1c476fa3ef
John Illsley <jsi22@bath.ac.uk>
2018-11-13 16:41:22 +0000

    Updates for demo

---
+++ lang/en/grouppeerreview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'grouppeerreview', language 'en', branch 'MOODLE_30_STABLE'
+ *
+ * @package mod_grouppeerreview
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['closebeforeopen'] = 'You have specified an end date before the start date.';
+$string['completionsubmit'] = 'View as completed if the peer review is submitted';
+$string['description'] = 'Description';
+$string['instructions'] = 'Student instructions and marking scheme for group peer review';
+$string['groupmark'] = 'Original group mark decided by marker';
+$string['modulename'] = 'Group peer review';
+$string['modulenameplural'] = 'Group peer reviews';
+$string['name'] = 'Name';
+$string['noresultsviewable'] = 'The results are not currently viewable.';
+$string['peerclose'] = 'Review start time';
+$string['peeropen'] = 'Review end time';
+$string['peersaved'] = 'Your review grades and comments have been saved';
+$string['pluginadministration'] = 'Peer review administration';
+$string['pluginname'] = 'Group peer review';
+$string['removeresponses'] = 'Remove all responses';
+$string['responses'] = 'Responses';
+$string['savereview'] = 'Save review';
+$string['selectassignment'] = 'Select group assignment';
+$string['selectgrouping'] = 'Select grouping';
+$string['viewallresponses'] = 'View {$a} responses';
+$string['weighting'] = 'Peer review weighting (%)';
+$string['instructions_default'] = '<h5 style="color: #D27">Instructions for Peer Assessments</h5><p></p>
+<ul><li>Scroll down to the form where you can enter the ratings for the members of your group.</li>
+<li>You must assess everyone in your group.</li>
+<li>Read over the criteria for peer assessments carefully. You are expected to follow them faithfully in your judgements.</li>
+<li>Your judgements should be careful and fair. They should be unaffected by how much you like or dislike others or their points of view, just as you presumably would want others to use impartial standards when evaluating you.</li>
+<li>These evaluations will be absolutely confidential. No student will have any access to your evaluations at any time. Each student will be informed about the overall picture of the evaluations she or he has received from others. No student should ask any other student about these evaluations.<br></li>
+</ul><h5 style="color: #D27">Criteria for Peer Assessments</h5><p></p><p></p>
+<ul><li><b><i>Does Not Meet Expectations</i>: (lowest grade 1)
+<br></b>This person contributed the least amount of ideas and work, significantly below others in the group.</li>
+<li><span><i style="font-weight: bold;">Meets Expectations</i><b>: (middle grade 3)
+<br></b></span>Made a fair and average contribution to the paper.</li>
+<li><span><i style="font-weight: bold;">Exceeds Expectations</i><b>: (highest grade 5)
+<br></b></span>Paper reflects more of this person\'s ideas and work than that of anyone else in the group.</li>
+</ul><p></p>';
+
+$string['selectassignment_help'] = 'Only assignments that are configured for group submission are listed here. The grouping selected
+ for the assignment is inherited for the group peer review.';
+$string['weighting_help'] = '<strong>For example</strong><br/>If you select 20% then the final mark would comprise of...<br/><br/>
+80% of the group mark set by the marker <strong>PLUS</strong> 20% of the peer review mark set by other members of their group. 
+This assumes that both the group mark and the peer review mark have the same maximum mark.';
+$string['modulename_help'] = 'The group peer review activity can be used alongside a Moodle assignment that has been configured for
+ group submission. The group peer review activity allows members of each submission group to rate each others contribution to the
+ group project. It also has an option to allow students to assess their own input to the group assignment.';
